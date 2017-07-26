7f9f1e5c6128aaa9cfa331a79451c2a77051f235
Per Abich <per.abich@ig.com>
2017-07-26 09:03:58 +0100

    First nearly working version of terraformed environment

---
+++ terraform/consul.tf
@@ -0,0 +1,86 @@
+provider "aws" {
+  access_key = "${var.access_key}"
+  secret_key = "${var.secret_key}"
+  region     = "${var.region}"
+}
+
+data "aws_ami" "consul" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["packer-consul*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["self"]
+}
+
+resource "aws_instance" "consul-server" {
+  ami           = "${data.aws_ami.consul.id}"
+  instance_type = "t2.nano"
+  iam_instance_profile = "${aws_iam_instance_profile.consul-join.name}"
+  tags {
+    Name = "consul-server"
+  }
+}
+
+# Create the policy
+data "aws_iam_policy_document" "consul-join-policy" {
+  statement {
+    sid=1
+
+    actions = [
+      "ec2:DescribeInstances",
+    ]
+
+    resources = [
+      "*",
+    ]
+  }
+}
+
+resource "aws_iam_policy" "consul-join" {
+  name        = "${var.namespace}-consul-join"
+  description = "Allows Consul nodes to describe instances for joining."
+  policy      = "${data.aws_iam_policy_document.consul-join-policy.json}"
+}
+
+# Create an IAM role for the auto-join
+# Create the policy
+data "aws_iam_policy_document" "consul-join-role" {
+  statement {
+    sid=1
+
+    actions = [
+      "sts:AssumeRole",
+    ]
+
+    principals {
+      type= "Service"
+      identifiers = ["ec2.amazonaws.com"]
+    }
+  }
+}
+
+resource "aws_iam_role" "consul-join" {
+  name               = "${var.namespace}-consul-join"
+  assume_role_policy = "${data.aws_iam_policy_document.consul-join-role.json}"
+}
+
+# Attach the policy
+resource "aws_iam_policy_attachment" "consul-join" {
+  name       = "${var.namespace}-consul-join"
+  roles       = ["${aws_iam_role.consul-join.name}"]
+  policy_arn = "${aws_iam_policy.consul-join.arn}"
+}
+
+# Create the instance profile
+resource "aws_iam_instance_profile" "consul-join" {
+  name  = "${var.namespace}-consul-join"
+  role  = "${aws_iam_role.consul-join.name}"
+}
---
+++ terraform/variables.tf
@@ -0,0 +1,15 @@
+variable "access_key" {}
+variable "secret_key" {}
+variable "region" {
+  default = "eu-central-1"
+}
+
+variable "namespace" {
+  description = <<EOH
+The namespace to create the virtual training lab. This should describe the
+training and must be unique to all current trainings. IAM users, workstations,
+and resources will be scoped under this namespace.
+It is best if you add this to your .tfvars file so you do not need to type
+it manually with each run
+EOH
+}
